8c1d2615d8f434c6f48157b58956dfca80a7c654
MrQuatrelle <maigasmendo@gmail.com>
2023-06-15 00:43:28 +0100

    compressing and writing to file workinggit add main.cgit add main.cgit add main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+#include <sys/cdefs.h>
 
 FILE* input_fd;
 FILE* output_fd;
 
 huff_node_t* byte_heap;
+int byte_heap_depth = 0;
 
 void print_usage(char* program_name) {
     printf("huffman usage:\n"
@@ -62,14 +64,15 @@ void sort_nodes(huff_node_t* node_array[], int length) {
     }
 }
 
-void associate_codes_to_bytes(huff_node_t* head, unsigned int code) {
+void associate_codes_to_bytes(huff_node_t* head, unsigned code, int depth) {
     head->code = code;
+    byte_heap_depth = (depth > byte_heap_depth) ? depth : byte_heap_depth;
 
     if (head->left)
-        associate_codes_to_bytes(head->left, code << 1);
+        associate_codes_to_bytes(head->left, code << 1, depth + 1);
 
     if (head->right)
-        associate_codes_to_bytes(head->right, (code << 1) + 1);
+        associate_codes_to_bytes(head->right, (code << 1) + 1, depth + 1);
 }
 
 void build_heap(huff_node_t** heap, int length) {
@@ -88,13 +91,15 @@ void build_heap(huff_node_t** heap, int length) {
     } while (heap[1] != NULL); // while length > 1 (in the end there
                                // will only be the head of the heap in
                                // the list)
-    associate_codes_to_bytes(heap[0], 1);
-    print_heap(heap[0], 0);
+    associate_codes_to_bytes(heap[0], 0b1, 1);
+
+    // // TODO: remove debugging
+    // print_heap(heap[0], 0);
 
     byte_heap = heap[0];
 }
 
-void prepare_nodes() {
+huff_node_t** prepare_nodes() {
     huff_node_t** byte_nodes = malloc(sizeof(huff_node_t*) * BYTE_ARRAY_LENGTH);
     assert(byte_nodes);
 
@@ -121,9 +126,49 @@ void prepare_nodes() {
     }
 
     build_heap(buffer, unique_bytes);
-
-    free(byte_nodes);
     free(buffer);
+
+    return byte_nodes;
+}
+
+__always_inline unsigned int get_code(int c, huff_node_t** key_stack) {
+    return key_stack[c]->code;
+}
+
+void generate_compressed_content(huff_node_t** key_stack) {
+    // TODO: how the hell am I going to save the encoding in the file???
+
+    char c;
+    char buffer = 0;
+    char counter = 0;
+    while ((c = fgetc(input_fd)) != EOF) {
+        unsigned int mask = 1 << 31;
+        unsigned int key = get_code(c, key_stack);
+
+        // advance until the start of the code
+        while (!(key & mask))
+            mask >>= 1;
+        mask >>= 1;
+        //
+
+        while (mask) {
+            if (mask & key)
+                buffer |= 1;
+            counter++;
+
+            if (counter == 8) {
+                fputc(buffer, output_fd);
+                counter = 0;
+                buffer = 0;
+            } else {
+                buffer <<= 1;
+            }
+
+            mask >>= 1;
+        }
+    }
+    if (counter)
+        fputc(buffer, output_fd);
 }
 
 int main(int argc, char* argv[]) {
@@ -145,8 +190,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    prepare_nodes();
+    huff_node_t** key_stack = prepare_nodes();
+
     fseek(input_fd, 0, SEEK_SET);
+
+    generate_compressed_content(key_stack);
 
     fclose(input_fd);
     fclose(output_fd);
